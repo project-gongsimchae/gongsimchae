84c1f02a198d8fdd9133d37c6eda526115aee36b
yshong1998 <yshong1998@gmail.com>
2024-07-30 15:48:12 +0900

    Feat: 카테고리 엔티티 생성

---
+++ src/main/java/techit/gongsimchae/domain/category/Category.java
@@ -0,0 +1,22 @@
+package techit.gongsimchae.domain.category;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Category {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private Long categoryNumber;
+
+    public Category (String name, Long categoryNumber){
+        this.name = name;
+        this.categoryNumber = categoryNumber;
+    }
+}
