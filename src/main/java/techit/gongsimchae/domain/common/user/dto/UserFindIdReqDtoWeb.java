8011351dda4b8cbae77dda5776953a01a618b4e0
param <spsummerdoc@gmail.com>
2024-07-30 17:36:15 +0900

    Feat: 아이디 찾기, 비밀번호 찾기 로직 구현

---
+++ src/main/java/techit/gongsimchae/domain/common/user/dto/UserFindIdReqDtoWeb.java
@@ -0,0 +1,9 @@
+package techit.gongsimchae.domain.common.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserFindIdReqDtoWeb {
+    private String name;
+    private String email;
+}
--- src/main/java/techit/gongsimchae/domain/mail/service/EmailService.java
+++ src/main/java/techit/gongsimchae/domain/mail/service/EmailService.java
@@ -2,14 +2,18 @@ package techit.gongsimchae.domain.mail.service;
 
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import techit.gongsimchae.domain.mail.event.AuthCodeEvent;
 import techit.gongsimchae.domain.mail.event.JoinMailEvent;
+import techit.gongsimchae.domain.mail.event.LoginIdEvent;
+import techit.gongsimchae.domain.mail.event.PasswordEvent;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
     private final JavaMailSender javaMailSender;
 
@@ -26,6 +30,24 @@ public class EmailService {
         message.setSubject("[공심채] 인증번호입니다.");
         message.setTo(event.getEmail());
         message.setText("인증 코드는 "+event.getCode()+ " 입니다.");
+        javaMailSender.send(message);
+    }
+
+    public void sendPasswordEmail(PasswordEvent event) {
+        log.debug("send passwordEvent {} ", event);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("[공심채] 임시 비밀번호입니다.");
+        message.setTo(event.getEmail());
+        message.setText("비밀번호는 "+event.getPassword()+ " 입니다.");
+        javaMailSender.send(message);
+    }
+
+    public void sendLoginId(LoginIdEvent event) {
+        log.debug("send LoginIdEvent {} ", event);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("[공심채] 아이디를 안내드립니다.");
+        message.setTo(event.getEmail());
+        message.setText("아이디는 "+event.getLoginId()+ " 입니다.");
         javaMailSender.send(message);
     }
 }
