526bbe1bb50c66019258def7605b245a5bde630f
yshong1998 <yshong1998@gmail.com>
2024-08-05 17:48:42 +0900

    fix : ItemController에서 관리자 관련 기능 매핑 추가

--- src/main/java/techit/gongsimchae/web/ItemController.java
+++ src/main/java/techit/gongsimchae/web/ItemController.java
@@ -21,13 +21,13 @@ public class ItemController {
     private final ItemService itemService;
     private final CategoryService categoryService;
 
-    @GetMapping
+    @GetMapping("/admin/item")
     public String listItems(Model model) {
         model.addAttribute("items", itemService.getAllItems());
         return "admin/item/item";
     }
 
-    @GetMapping("/create")
+    @GetMapping("/admin/item/create")
     public String showItemForm(Model model) {
         model.addAttribute("item", new ItemCreateDto());
         model.addAttribute("categories", categoryService.getAllCategories());
@@ -35,13 +35,13 @@ public class ItemController {
         return "admin/item/createForm";
     }
 
-    @PostMapping("/create")
+    @PostMapping("/admin/item/create")
     public String createItem(@ModelAttribute ItemCreateDto itemCreateDto) {
         itemService.createItem(itemCreateDto);
         return "redirect:/admin/item";
     }
 
-    @GetMapping("/update/{id}")
+    @GetMapping("/admin/item/update/{id}")
     public String showUpdateForm(@PathVariable Long id, Model model){
         Item item = itemService.getItemById(id);
         if(item == null) {
@@ -53,13 +53,13 @@ public class ItemController {
         return "admin/item/updateForm";
     }
 
-    @PostMapping("/update/{id}")
+    @PostMapping("/admin/item/update/{id}")
     public String updateItem(@PathVariable Long id, @ModelAttribute ItemUpdateDto itemUpdateDto){
         itemService.updateItem(id, itemUpdateDto);
         return "redirect:/admin/item";
     }
 
-    @PostMapping("/delete")
+    @PostMapping("/admin/item/delete")
     public String deleteItem(@RequestParam Long id){
         itemService.deleteItem(id);
         return "redirect:/admin/item";
