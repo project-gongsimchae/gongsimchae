59a0215aad0e6998485db38df3fd3f0572bab190
yshong1998 <yshong1998@gmail.com>
2024-08-09 15:12:48 +0900

    Feat: 에러, 성공 메세지 관리 enum 클래스 생성

---
+++ src/main/java/techit/gongsimchae/global/message/ErrorMessage.java
@@ -0,0 +1,32 @@
+package techit.gongsimchae.global.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorMessage {
+
+    /**
+     * common 에러
+     */
+
+    /**
+     * groupBuying 에러
+     */
+
+    EVENT_TYPE_NOT_VALID("유효하지 않은 이벤트 타입입니다.", "G001" );
+
+    /**
+     * portion 에러
+     */
+
+
+    /**
+     * global 에러
+     */
+
+    private final String message;
+    private final String customStatusCode;
+
+}
