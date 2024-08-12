49776cdf8e21aef9685b7786a1081e83c7a223e6
suynnn <suy00065@gmail.com>
2024-08-12 10:52:06 +0900

    Refactor: 각 소분글에 연관된 이미지 파일들 LEFT JOIN으로 같이 가져오게 변경

--- src/main/java/techit/gongsimchae/domain/portion/subdivision/repository/SubdivisionRepository.java
+++ src/main/java/techit/gongsimchae/domain/portion/subdivision/repository/SubdivisionRepository.java
@@ -1,6 +1,8 @@
 package techit.gongsimchae.domain.portion.subdivision.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import techit.gongsimchae.domain.portion.subdivision.entity.Subdivision;
 
@@ -10,7 +12,10 @@ import java.util.Optional;
 @Repository
 public interface SubdivisionRepository extends JpaRepository<Subdivision, Long> {
 
-    Optional<Subdivision> findByUID(String UID);
+    @Query("SELECT s FROM Subdivision s LEFT JOIN FETCH s.imageFileList WHERE s.UID = :uid")
+    Optional<Subdivision> findByUID(@Param("uid") String UID);
+
+    @Query("SELECT s FROM Subdivision s LEFT JOIN FETCH s.imageFileList")
     List<Subdivision> findByOrderByCreateDateDesc();
 
     List<Subdivision> findAllByUserId(Long userId);
