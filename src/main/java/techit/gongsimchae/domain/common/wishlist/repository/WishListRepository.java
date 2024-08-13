757a167ab33d819360c9d2c96dd86ccd96fac26d
suynnn <suy00065@gmail.com>
2024-08-13 10:33:34 +0900

    Fix: 소분글 관심목록 안불러지는 문제 임시 해결

--- src/main/java/techit/gongsimchae/domain/common/wishlist/repository/WishListRepository.java
+++ src/main/java/techit/gongsimchae/domain/common/wishlist/repository/WishListRepository.java
@@ -1,6 +1,8 @@
 package techit.gongsimchae.domain.common.wishlist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import techit.gongsimchae.domain.common.wishlist.entity.WishList;
 
@@ -9,5 +11,6 @@ import java.util.List;
 @Repository
 public interface WishListRepository extends JpaRepository <WishList, Long> {
 
-    List<WishList> findWishListsByUserIdAndItemIsNull(Long userId);
+    @Query("SELECT wl FROM WishList wl LEFT JOIN FETCH wl.subdivision s LEFT JOIN FETCH s.imageFileList WHERE wl.user.id = :userId AND wl.item IS NULL")
+    List<WishList> findWishListsByUserIdAndItemIsNull(@Param("userId") Long userId);
 }
