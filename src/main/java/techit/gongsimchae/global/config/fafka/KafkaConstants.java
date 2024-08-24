3ed8f600041426caa239013479322e475457feb7
suynnn <suy00065@gmail.com>
2024-08-24 10:58:33 +0900

    Config: 메인브랜치에서는 카프카 접속할때 로컬이 아니라 카프카 서버에 접속할 수 있도록 재설정

--- src/main/java/techit/gongsimchae/global/config/fafka/KafkaConstants.java
+++ src/main/java/techit/gongsimchae/global/config/fafka/KafkaConstants.java
@@ -6,6 +6,6 @@ public class KafkaConstants {
     public static final String KAFKA_AI_TOPIC = "chat-ai-topic";
     public static final String KAFKA_TOPIC = "chat-topic";
     public static final String GROUP_ID = "chat-group";
-    public static final String KAFKA_BROKER = "localhost:9092";
+    public static final String KAFKA_BROKER = "13.125.196.230:9092";
     public static List<Integer> partitionList;
 }
