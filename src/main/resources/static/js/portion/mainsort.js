92c7bc4d62edd655ac5786ef3825739ca4fa2779
param <spsummerdoc@gmail.com>
2024-08-29 13:28:59 +0900

    Fix: 소분 검색버튼을 안눌러도 ajax로 데이터가 날라가는 로직 변경

--- src/main/resources/static/js/portion/mainsort.js
+++ src/main/resources/static/js/portion/mainsort.js
@@ -14,14 +14,6 @@ function updateURLAndFetch(page) {
         }
     }
 
-    // 검색어 추가
-    let query = document.querySelector('.content-wrapper input').value;
-    if (query) {
-        url.searchParams.set('query', query);
-    } else {
-        url.searchParams.delete('query');
-    }
-
     // 판매중 체크박스 상태 반영
     let onSaleChecked = document.getElementById('onSaleCheckbox').checked;
     if (onSaleChecked) {
@@ -99,22 +91,9 @@ document.getElementById('sortPopular').addEventListener('click', function(e) {
     }
 });
 
-// 검색어 입력 필드에 입력할 때마다 URL 업데이트 및 데이터 가져오기
-let debounceTimer;
-document.querySelector('.content-wrapper input').addEventListener('input', function() {
-    clearTimeout(debounceTimer);
-    debounceTimer = setTimeout(updateURLAndFetch, 300); // 300ms 디바운스
-});
-
 // 페이지 로드 시 URL 파라미터에 따라 UI 상태 설정
 window.addEventListener('load', function() {
     let params = new URLSearchParams(window.location.search);
-
-    // 검색어 설정
-    let query = params.get('query');
-    if (query) {
-        document.querySelector('.content-wrapper input').value = query;
-    }
 
     // 판매중 체크박스 설정
     let onSale = params.get('onSale');
