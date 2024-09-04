52a53ced71a4e62a3a918078d9db94ee514ae20c
suynnn <suy00065@gmail.com>
2024-09-04 21:59:20 +0900

    Fix: 원래 코드로 다시 수정

--- src/main/java/techit/gongsimchae/web/WebRestController.java
+++ src/main/java/techit/gongsimchae/web/WebRestController.java
@@ -5,6 +5,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 @AllArgsConstructor
 public class WebRestController {
@@ -13,12 +15,8 @@ public class WebRestController {
 
     @GetMapping("/profile")
     public String getProfile () {
-        String[] profiles = env.getActiveProfiles();
-
-        if (profiles.length > 1) {
-            return profiles[1];  // 두 번째 프로파일 반환
-        } else {
-            return profiles.length > 0 ? profiles[0] : "";
-        }
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
