91972dbe4d42cf0c348e69832f882e37bb42e80a
suynnn <suy00065@gmail.com>
2024-09-05 14:17:53 +0900

    Refactor : 결제 후 달성량 체크해서 공동구매 목표량 달성했다면 공동구매 성공 이벤트 발행

--- src/main/java/techit/gongsimchae/domain/groupbuying/payment/service/PaymentsService.java
+++ src/main/java/techit/gongsimchae/domain/groupbuying/payment/service/PaymentsService.java
@@ -7,19 +7,26 @@ import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import techit.gongsimchae.domain.common.user.entity.User;
 import techit.gongsimchae.domain.common.user.repository.UserRepository;
+import techit.gongsimchae.domain.common.user.service.UserService;
 import techit.gongsimchae.domain.groupbuying.cart.service.CartService;
+import techit.gongsimchae.domain.groupbuying.item.entity.Item;
 import techit.gongsimchae.domain.groupbuying.itemoption.service.ItemOptionService;
 import techit.gongsimchae.domain.groupbuying.orderitem.entity.OrderItem;
+import techit.gongsimchae.domain.groupbuying.orderitem.entity.OrderItemStatus;
 import techit.gongsimchae.domain.groupbuying.orderitem.entity.OrderStatus;
 import techit.gongsimchae.domain.groupbuying.orderitem.repository.OrderItemRepository;
+import techit.gongsimchae.domain.groupbuying.orderitem.service.OrderItemService;
 import techit.gongsimchae.domain.groupbuying.orders.dto.TempOrderItemDto;
 import techit.gongsimchae.domain.groupbuying.orders.entity.Orders;
 import techit.gongsimchae.domain.groupbuying.orders.repository.OrdersRepository;
 import techit.gongsimchae.domain.groupbuying.payment.dto.PaymentDto;
+import techit.gongsimchae.domain.portion.notifications.event.GroupBuyingNotiEvent;
+import techit.gongsimchae.global.event.TargetCountAchievedEvent;
 import techit.gongsimchae.global.exception.CustomWebException;
 import techit.gongsimchae.global.message.ErrorMessage;
 
@@ -39,6 +46,10 @@ public class PaymentsService {
     private final ItemOptionService itemOptionService;
     private final OrderItemRepository orderItemRepository;
     private final CartService cartService;
+    private final OrderItemService orderItemService;
+    private final UserService userService;
+    private final ApplicationEventPublisher publisher;
+
     public List<PaymentDto> pgProvider(){
         return Arrays.asList(
                 new PaymentDto("1", "카카오페이", "kakaopay.TC0ONETIME"),
@@ -110,6 +121,10 @@ public class PaymentsService {
         completeOrderStatus(payment);
         cartService.removeMultipleItems(userId, tempOrderItems.stream()
                 .map(item -> item.getItemOptionId()).collect(Collectors.toList()));
+
+        changeOrderItemStatus(order);
+        checkTheTargetAmountAchieved(order);
+
         return paymentResponse;
     }
 
@@ -144,6 +159,53 @@ public class PaymentsService {
             iamportClient.cancelPaymentByImpUid(cancelData);
         } catch (IamportResponseException | IOException e) {
             throw new RuntimeException();
+        }
+    }
+
+    /**
+     * OrderItem의 상태를 결제완료로 바꾸는 메서드입니다.
+     *
+     * @param orders
+     */
+    private void changeOrderItemStatus(Orders orders) {
+        List<OrderItem> orderItems = orderItemRepository.findAllByOrders(orders);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.updateOrderItemStatus(OrderItemStatus.결제완료);
+        }
+    }
+
+    /**
+     * 각 아이템에 대한 주문량이 목표량에 도달했는지 체크하는 메서드입니다.
+     *
+     * @param orders
+     */
+    private void checkTheTargetAmountAchieved(Orders orders) {
+        List<OrderItem> orderItems = orderItemRepository.findAllByOrders(orders);
+
+        for (OrderItem orderItem : orderItems) {
+            Item item = orderItem.getItemOption().getItem();
+
+            if (item.getGroupBuyingQuantity() <= orderItemService.getCountCompletedOrderItemsWithItemId(item.getId()).getCompletionAmountCnt()) {
+                groupBuyingSuccess(item);
+            }
+        }
+    }
+
+    /**
+     * 주문량이 목표량에 도달했다면 공동구매 성공 이벤트 및 성공 알림 이벤트를 발행시킵니다.
+     *
+     * @param item
+     */
+    private void groupBuyingSuccess(Item item) {
+        List<User> users = userService.findUsersByItemId(item.getId());
+        log.info("usersize : {}", users.size());
+
+        publisher.publishEvent(new TargetCountAchievedEvent(item.getId()));
+
+        for (User user : users) {
+            log.debug("group buying event");
+            publisher.publishEvent(new GroupBuyingNotiEvent(user, item.getName() + "에 대한 공동구매가 성공했습니다! 배송 준비 중에 있으니 잠시만 기다려주세요."));
         }
     }
 }
